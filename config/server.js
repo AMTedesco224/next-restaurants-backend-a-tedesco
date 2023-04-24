2e974020621e1214bf46bf617d4b888e6d499dbe
Anthony Tedesco <amtedesco224@yahoo.com>
2023-04-24 17:50:44 -0400

    submission commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f502179b857c16e26aa7feb69a87ce4f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '10d2b869-416a-41a2-a4a4-162238e61d87'
+};
